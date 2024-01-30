3da534a7a88fede8b7853dc89faeb860631af80b
gauravbordoloi <gmonetix@gmail.com>
2024-01-30 15:26:26 +0530

    fixes

--- index.js
+++ index.js
@@ -1,28 +1,63 @@
-const { XMLParser, XMLBuilder, XMLValidator } = require("fast-xml-parser")
-const { translate } = require("./translate-api")
-const { join } = require('path')
-const fs = require("fs");
+import { XMLParser, XMLBuilder, XMLValidator } from "fast-xml-parser"
+import { translate } from "./translate-api.js"
+import { join } from 'path'
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs"
+import Languages from "./languages.js"
+import builder from 'xmlbuilder'
 
+/**
+ * -------------Define project data----------------------
+ */
 const projectPath = "E:\\android-store-maintenance"
-const stringsPath = join(projectPath, "app", "src", "main", "res", "values", "strings.xml")
+const targetLanguages = [
+    Languages.Assamese,
+    Languages.Bengali,
+    Languages.Gujarati,
+    Languages.Hindi,
+    Languages["Odia (Oriya)"],
+    Languages.Kannada,
+    Languages.Malayalam,
+    Languages.Marathi,
+    Languages.Punjabi,
+    Languages.Tamil,
+    Languages.Telugu
+]
+const sourceLang = "en"
 
+//------------------------------------------------------
+
+const stringsPath = join(projectPath, "app", "src", "main", "res", "values", "strings.xml")
 const parser = new XMLParser({
     preserveOrder: true,
-    ignoreAttributes: false,
-    attributeNamePrefix: "",
-
+    ignoreAttributes: false
 });
-let jObj = parser.parse(fs.readFileSync(stringsPath, "utf-8"));
+let originalObj = parser.parse(readFileSync(stringsPath, "utf-8"));
+writeFileSync("t.json", JSON.stringify(originalObj))
 
-let map = {}
-jObj[0].resources.forEach(o => {
+let source = {}
+originalObj[0].resources.forEach(o => {
     if (o.string) {
-        map[`${o[":@"]["name"]}`] = `${o.string[0]["#text"]}`
+        source[`${o[":@"]["@_name"]}`] = `${o.string[0]["#text"]}`
     }
 })
 
-fs.writeFileSync("test.json", JSON.stringify(map))
+for (let i = 0; i < targetLanguages.length; i++) {
+    const lang = targetLanguages[i];
+    const langValues = { ...source }
 
-// const builder = new XMLBuilder();
-// const xmlContent = builder.build(jObj);
+    let values = await translate(Object.values(langValues), lang, sourceLang)
+
+    const root = builder.begin().ele('resources');
+
+    Object.keys(langValues).forEach((k, i) => {
+        root.ele('string', { 'name': k }, values[i])
+    })
+
+    const xml = root.end({ pretty: true });
+    const _path = join(projectPath, "app", "src", "main", "res", `values-${lang}`)
+    if (!existsSync(_path)) {
+        mkdirSync(_path)
+    }
+    writeFileSync(join(_path, "strings.xml"), xml)
+}
 
--- translate-api.js
+++ translate-api.js
@@ -1,4 +1,5 @@
-const axios = require('axios').default
+import axios from 'axios'
+import { writeFileSync } from 'fs'
 
 const translate = async (texts, targetLang, sourceLang) => {
     if (!texts || !Array.isArray(texts) || texts.length === 0) {
@@ -9,21 +10,29 @@ const translate = async (texts, targetLang, sourceLang) => {
     }
     const responses = []
     do {
-        let res = await axios.get(
-            "https://translation.googleapis.com/language/translate/v2",
-            {
-                params: {
-                    q: texts.splice(0, 128),
-                    "target": targetLang,
-                    "source": sourceLang,
-                    "key": process.env.API_KEY
+        try {
+            const params = new URLSearchParams();
+            texts.splice(0, 128).forEach(t => params.append("q", t))
+            params.append("target", targetLang);
+            params.append("source", sourceLang);
+            params.append("key", process.env.API_KEY);
+            let res = await axios.get(
+                "https://translation.googleapis.com/language/translate/v2",
+                {
+                    params
                 }
+            )
+            if (res.status === 200 && res.data?.data?.translations && Array.isArray(res.data.data.translations) && res.data.data.translations.length > 0) {
+                writeFileSync("p.json", JSON.stringify(res.data.data.translations))
+                responses.push(...res.data.data.translations.map(t => t.translatedText))
+            } else {
+                throw new Error("Error with status - " + responses.status)
             }
-        )
-        if (res.status === 200 && res.data?.data?.translations && Array.isArray(res.data.data.translations) && res.data.data.translations.length > 0) {
-            responses.push(res.data.data.translations.map(t => t.translatedText))
+        } catch (error) {
+            throw new Error(error.message)
         }
-    } while (texts.length > 0);
+    } while (texts.length > 0)
+    return responses;
 }
 
-module.exports = { translate }
+export { translate }
